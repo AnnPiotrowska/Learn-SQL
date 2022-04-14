53002112eeccad83d85d4007e6de4b9820beedee
Ann.Piotrowska <anna.piotrowska.pl@gmail.com>
2022-04-14 09:39:13 +0200

    Update file SQL_basic_statements.sql

--- SQL-from-LNU-platform/SQL_basic_statements.sql
+++ SQL-from-LNU-platform/SQL_basic_statements.sql
@@ -1,27 +1,26 @@
---Course SQL Level 1 and SQL Level 2 from LNU platform
---Learning Notes
---SQLite
-
 ----------------------
 --Basic SQL statements
 ----------------------
 
+--------------
 --LIMIT clause
+--------------
 
---dane dziesieciu najdrozszych modeli telefonow
+--ten most expensive phone models
 SELECT * 
 FROM cell_phones 
 ORDER BY price DESC 
 LIMIT 0,10;
 
---wybrac rekordy od miejsca 21 do 30
+--select records from 21st to 30th
 SELECT brand, model, price 
 FROM cell_phones 
 ORDER BY price DESC 
 LIMIT 20,10;     
 
-
+--------------
 --WHERE clause
+--------------
 
 SELECT * 
 FROM cell_phones 
@@ -29,7 +28,7 @@ WHERE color = 'black' AND price <= 300;
 
 SELECT title, year 
 FROM movies 
-WHERE year BETWEEN '1990' AND '1999'  --obejmuje zakres pomiedzy podanymi wartosciami wlacznie z nimi
+WHERE year BETWEEN '1990' AND '1999'  --covers the range between and including the given values
 ORDER BY year;
 
 SELECT * 
@@ -44,143 +43,144 @@ SELECT *
 FROM famous_cats 
 WHERE origin NOT IN ('Garfield', 'Smurfs', 'Tom & Jerry');
 
-
 --Subqueries
 
---z tabeli cell_phones wybierz telefony Twoich ulubionych marek
+--select the phones of your favorite brands from the cell_phones table
 SELECT * 
 FROM cell_phones 
 WHERE brand IN ( SELECT brand FROM favorite_brands );
 
--- z tabeli movies wyswietl wszystkie filmy, ktorych id nie ma w tabeli my_movies
+--from the movies table, display all movies that aren't in the my_movies table
 SELECT * 
 FROM movies 
 WHERE id NOT IN (SELECT id FROM my_movies); 
 
-
+-------------------------
 --LIKE/ NOT LIKE operator
+-------------------------
 --operator that determines if a character string matches a specified pattern
 
 --Wildcards:
 -- %(percent) represents any string of zero or more characters
 -- _(underscore) represents any single character
 
---filmy, ktorych tytuly zaczynaja sie od litery 'A'
+--movies with titles beginning with the letter 'A'
 SELECT * 
 FROM movies 
 WHERE title LIKE 'A%';
 
---w ktorych druga litera nazwiska rezysera to 'r’
+--movies where the second letter of the director's surname is 'r'
 SELECT * 
 FROM movies 
 WHERE director LIKE '_r%';
 
---z tabeli movies wybierz filmy, w ktorych zagral Clint Eastwood, ale ktorych nie rezyserowal
+--movies that Clint Eastwood has starred in but didn't direct
 SELECT * 
 FROM movies 
 WHERE [cast] LIKE '%Clint Eastwood%' AND director <> 'Clint Eastwood';
 
---wybierz wszystkie filmy, w ktorych nie zagral Tom Cruise
+--movies in which Tom Cruise hasn't starred
 SELECT * 
 FROM movies 
 WHERE [cast] NOT LIKE '%Tom Cruise%';
 
---z tabeli movies wybierz filmy, ktore maja tytuly jednoczlonowe 
+--movies that have single-ended titles 
 SELECT * 
 FROM movies 
 WHERE title NOT LIKE '% %';
 
-
+---------------------
 --AGGREGATE FUNCTIONS
-
+---------------------
 --MAX () - returns the maximum value
 --MIN () - returns the minimum value
 --COUNT () returns the number of rows (NULL not included)
 --SUM () - returns the total
 --AVG () - returns the average
 
-
---oblicz ile rekordow z tabeli movies zawiera ocene (pole rating) mieszczaca sie w przedziale od 7.5 do 8.5 wlacznie z tymi wartosciami
+--calculate how many records from the movies table contain the rating in the range from 7.5 to 8.5, including these values
 SELECT COUNT(rating) 
 FROM movies 
 WHERE rating BETWEEN '7.5' AND '8.5';
 
---oblicz ile roznych marek (pole brand) zawiera tabela cell_phones
+--calculate how many different brands contains the cell_phones table 
 SELECT COUNT(DISTINCT brand) 
 FROM cell_phones;
 
---pobierz srednia cene telefonow marki Apple
+--calculate the average price of Apple's phones
 SELECT AVG(price) 
 FROM cell_phones 
 WHERE brand = 'Apple';
 
---wyswietl tytuly i oceny tych filmow z tabeli movies, ktorych ocena jest wieksza lub rowna sredniej z Twoich ocen wszystkich filmow z tabeli my_movies
+--display the titles and ratings of movies with a rating greater than or equal to the average of your ratings for all movies in the my_movies table
 SELECT title,rating 
 FROM movies 
 WHERE rating >= (SELECT AVG(my_rating) FROM my_movies) 
 ORDER BY rating DESC;
 
---obliczenia sumarycznej wartosci mandatow wystawionych w Chicago
+--calculate the total value of tickets issued in Chicago
 SELECT SUM(fine) 
 FROM tickets 
 WHERE location ='Chicago';
 
---oblicz na jaka sume dostal mandaty Fletcher Reede w Los Angeles
+--calculate how much fine Fletcher Reede got in Los Angeles
 SELECT SUM(fine) 
 FROM tickets 
 WHERE location = 'Los Angeles' AND driver_id = (select ID from drivers where first_name = "Fletcher" AND family_name = "Reede");
 
---najnizsza cene sposrod modeli telefonow z systemem operacyjnym innym niz Android
+--calculate the lowest price for phone models with an operating system other than Android
 SELECT MIN(price) 
 FROM cell_phones 
 WHERE system <> 'Android'; 		--WHERE system NOT LIKE 'Android'
 
---rozmiar najwiekszego ekranu sposrod modeli, ktore nie sa koloru czerwonego, niebieskiego ani rozowego;
-SELECT MAX(screen) 
+--find the size of the largest screen of the models that are not red, blue or pink
+SELECT MAX(screen)
 FROM cell_phones 
 WHERE color NOT IN ('red','blue','pink');
 
-
+----------
 --GROUP BY
+----------
 
---ilu bohaterow z poszczegolnych bajek zawiera tabela famous_cats
+--how many heroes from each fairy tale contains the famous_cats table
 SELECT origin, COUNT(id) 
 FROM famous_cats 
 GROUP BY origin;
 
---z tabeli cell_phones uzyskaj liczby modeli dla poszczegolnych systemow operacyjnych, posortuj wyniki od grupy o najwiekszej liczbie modeli
+--from the cell_phones table, get the number of models for individual operating systems, sort the results from the group with the largest number of models
 SELECT system, COUNT(id) 
 FROM cell_phones 
 GROUP BY system 
 ORDER BY COUNT (id) DESC;
 
 
---ALIASES
+--Aliases
 
---wyswietl srednia cene dla modeli o tym samym rozmiarze ekranu
+--display the average price for models with the same screen size
 SELECT screen, AVG(price) AS avg_price
 FROM cell_phones 
 GROUP BY screen 
 ORDER BY avg_price DESC;
 
---z tabeli tickets wyswietl kolumny: location oraz sredni a wartosc wystawionych mandatow dla trzech  miejscowosci, w ktorych srednia wysokosc wystawionych mandatow jest najwyzsza
+--from the tickets table, display the columns: location and the average value of tickets for the three location where the average amount of tickets is the highest
 SELECT location, AVG(fine) AS [average fine] 
 FROM tickets 
 GROUP BY location 
 ORDER BY AVG(fine) DESC 
 LIMIT 0,3;
 
-
+--------
 --HAVING
+--------
 
---uwzgledniajac mandaty wystawione po roku 1991, oblicz sumy wartosci mandatow dla poszczegolnych miast, ale tylko tych, w ktorych wystawiono przynajmniej trzy mandaty
+--calculate the sum of the value of fines for individual cities, but only for those with at least three fines (include tickets issued after 1991)
 SELECT location, SUM(fine) AS total 
 FROM tickets 
-WHERE date >= '1992' 	--pole data zawiera dane typu text
+WHERE date >= '1992' 	--the field is of the text type
 GROUP BY location 
 HAVING COUNT(*)> 2;
 
---wyswietl dane tych kierowcow, dla ktorych suma wystawionych mandatow wynosi co najmniej 100 dolarow
+--display the details of those drivers with issued fines of at least 100 dollars
 SELECT driver_id, SUM(fine) AS [total fine] 
 FROM tickets 
 GROUP BY driver_id 
